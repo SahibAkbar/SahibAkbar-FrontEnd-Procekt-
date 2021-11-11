f7bf67b7fa51bfd96b5e9e9285686b00ecc84c8f
SahibAkbar <sahibaa@code.edu.az>
2021-11-12 02:23:04 +0400

    Added new pages(About,Services,Pricing)

---
+++ About/js/script.js
@@ -0,0 +1,70 @@
+window.addEventListener("scroll", (e) => {
+    if (window.pageYOffset > 100) {
+      document.querySelector(".navbar-area").classList.add("active"); 
+     } 
+     else {
+        document.querySelector(".navbar-area").classList.remove("active");
+      }
+  });
+
+
+
+
+
+  $('.myby').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000,
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  dots:true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+
+let Top = document.querySelector(".go-top")
+
+window.addEventListener("scroll",(e) =>{
+  if (window.pageYOffset > 500) {
+    Top.classList.add("active");
+  } else {
+    Top.classList.remove("active");
+  }
+});
+Top.addEventListener("click", (e) => {
+  window.scrollTo(0, 0);
+});
---
+++ Services/js/script.js
@@ -0,0 +1,101 @@
+window.addEventListener("scroll", (e) => {
+    if (window.pageYOffset > 100) {
+      document.querySelector(".navbar-area").classList.add("active"); 
+     } 
+     else {
+        document.querySelector(".navbar-area").classList.remove("active");
+      }
+  });
+
+
+
+
+
+  $('.myby').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000,
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay:true,
+  dots:true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+
+let Top = document.querySelector(".go-top")
+
+window.addEventListener("scroll",(e) =>{
+  if (window.pageYOffset > 500) {
+    Top.classList.add("active");
+  } else {
+    Top.classList.remove("active");
+  }
+});
+Top.addEventListener("click", (e) => {
+  window.scrollTo(0, 0);
+});
+
+
+
+let PricingTablePrivate = document.querySelectorAll(".pricing-table-private");
+let monthly = document.getElementById("monthly");
+let yearly = document.getElementById("yearly");
+
+yearly.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  monthly.style.color = "#8d8fb4";
+  monthly.style.backgroundColor = "#fff";
+  yearly.style.color = "#fff";
+  yearly.style.backgroundColor = "#080a3c";
+
+  for (let i = 0; i < PricingTablePrivate.length; i++) {
+    PricingTablePrivate[i].classList.add("active");
+  }
+});
+monthly.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  yearly.style.color = "#8d8fb4";
+  yearly.style.backgroundColor = "#fff";
+  monthly.style.color = "#fff";
+  monthly.style.backgroundColor = "#080a3c";
+
+  for (let i = 0; i < PricingTablePrivate.length; i++) {
+    PricingTablePrivate[i].classList.remove("active");
+  }
+});
